cdde911a716b6b07ed4140480b6b3c5f10842f9d
diathekez <96537263+diathekez@users.noreply.github.com>
2023-01-22 17:19:56 -0500

    Railway configuration.

--- config/database.js
+++ config/database.js
@@ -1,48 +1,49 @@
 // This script connects to MongoDB
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.DB_STRING, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useFindAndModify: false,
-//     });
-
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
-// This script connects to MongoDB on serverless cloud services
-const express = require("express");
 const mongoose = require("mongoose");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING);
+    const conn = await mongoose.connect(process.env.DB_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
 
+module.exports = connectDB;
+exports.connectDB = connectDB;
+
+// This script connects to MongoDB on serverless cloud services
+// const express = require("express");
+// const mongoose = require("mongoose");
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.DB_STRING);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
 //Routes go here
-app.all("*", (req, res) => {
-  res.json({ "every thing": "is awesome" });
-});
+// app.all("*", (req, res) => {
+//   res.json({ "every thing": "is awesome" });
+// });
 
 //Connect to the database before listening
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("listening for requests");
-  });
-});
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log("listening for requests");
+//   });
+// });
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 // Serverless connection
-const { MongoClient } = require("mongodb");
-const uri = process.env.DB_STRING;
-const client = new MongoClient(uri);
+// const { MongoClient } = require("mongodb");
+// const uri = process.env.DB_STRING;
+// const client = new MongoClient(uri);
 //
 
 const connectDB = require("./config/database");
@@ -30,25 +30,6 @@ app.use("/", homeRoutes);
 app.use("/todos", todoRoutes);
 
 // Heroku configuration
-// app.listen(process.env.PORT, () => {
-//   console.log("Server is running, homie.");
-// });
-
-// Cyclic configuration
-const dbS = connectDB().then(() => {
-  client.connect(async (err) => {
-    //Connect To Database
-    // connectDB();
-    // if(!connect) {console.log('Mongoose ran away'); return false}
-    if (err) {
-      console.error(err);
-      return false;
-    }
-    // connection to mongo is successful, listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log("Listening for requests");
-    });
-  });
-  // connectDB(app)
-  //
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, homie.");
 });
