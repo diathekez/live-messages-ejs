ca59d63b8bbfbe071cb95c86a694c6947ee509ea
diathekez <96537263+diathekez@users.noreply.github.com>
2023-01-21 23:38:11 -0500

    MongoDB connection configured for serverless function.

--- config/database.js
+++ config/database.js
@@ -1,19 +1,48 @@
 // This script connects to MongoDB
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.DB_STRING, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//     })
+
+//     console.log(`MongoDB Connected: ${conn.connection.host}`)
+//   } catch (err) {
+//     console.error(err)
+//     process.exit(1)
+//   }
+// }
+
+// module.exports = connectDB
+
+// This script connects to MongoDB on serverless cloud services
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
+    const conn = await mongoose.connect(process.env.DB_STRING);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+//Routes go here
+app.all("*", (req, res) => {
+  res.json({ "every thing": "is awesome" });
+});
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
